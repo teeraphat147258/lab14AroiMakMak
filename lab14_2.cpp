c382088d8825d3e72d0beb19b2139722123abfe2
teeraphat147258 <teeraphat147258@gmail.com>
2021-12-28 09:53:15 +0700

    lab14_2 completed

--- lab14_2.cpp
+++ lab14_2.cpp
@@ -21,3 +21,30 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void updateImage(bool d[N][M],int s,int x,int y){
+    
+    for(int N=0;N<30;N++){
+        for(int M=0;M<70;M++){
+            if(sqrt(pow(N-x,2)+pow(M-y,2))<=s-1){
+                d[N][M]=1;
+            }
+        }
+    }
+}
+
+void showImage(const bool e[][M]){
+    cout << "------------------------------------------------------------------------\n";
+    for(int N=0;N<30;N++){
+        cout << "|";
+        for(int M=0;M<70;M++){
+            if(e[N][M]==1){
+                cout << "*";
+            }else{
+                cout << " ";
+            }
+        }
+        cout << "|\n";
+    }
+cout << "------------------------------------------------------------------------\n";
+}
