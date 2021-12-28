e994d5bebdebea9e7cf82caaa0f120428d8bcc21
teeraphat147258 <teeraphat147258@gmail.com>
2021-12-28 09:54:15 +0700

    lab14_1 completed

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -17,3 +17,28 @@ int main(){
     cout << "\nMin = " << B[5];
     return 0;
 }
+
+
+void stat(const double A[],int N, double B[]){
+    double sum=0;
+    double std=0;
+    double geoM=1;
+    double HM=0;
+    double Max=A[0];
+    double Min=A[0];
+    for(int i=0;i<N;i++){
+        
+        sum = sum + A[i];
+        std += pow(A[i],2);
+        geoM = geoM*A[i];
+        HM = HM+(1/A[i]);
+        if(A[i] > Max) Max = A[i];
+        if(A[i] < Min) Min = A[i];
+    } 
+    B[0] = sum/N;
+    B[1] = sqrt((std/N)-pow(B[0],2));
+    B[2] = pow(geoM,pow(N,-1));
+    B[3] = N/HM;
+    B[4] = Max;
+    B[5] = Min;
+}
